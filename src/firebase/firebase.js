6abb6d125808654b252da0d0eff11c511352e42e
Your Name <jake.lower17@gmail.com>
2018-10-22 06:13:24 -0500

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,46 +13,4 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-// database.ref().on('value', (snapshot) => {
-//     console.log(snapshot.val());
-// });
-
-// database.ref()
-//     .once('value')
-//     .then((snapshot) => {
-//         const val = snapshot.val();
-//         console.log(val);
-//     }).catch((e) => {
-//         console.log('Error fetching data', e)
-//     });
-
-// database.ref().set({
-//     name: 'Jake',
-//     age: 26,
-//     isSingle: false,
-//     location: {
-//         city: 'Aledo',
-//         country: 'USA'
-//     }
-// }).then(() => {
-//     console.log('Data is saved.');
-// }).catch((e) => { 
-//     console.log('this failed');
-// });
-
-// database.ref().update({
-//     name: 'Mike',
-//     age: 40,
-//     job: 'progtramer',
-//     isSingle: null
-// });
-
-
-// console.log('I made req!');
-// // database.ref().set('This is my data');
-
-// database.ref('age').set(29);
-
-// database.ref('location/city').set('Joy');
-
-// database.ref('isSingle').remove();
+export { firebase, database as default };
