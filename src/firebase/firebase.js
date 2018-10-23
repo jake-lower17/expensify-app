b0b920e7f3d4ed1fd048a04c18dee5102baf542a
Jacob Lower <jake.lower17@gmail.com>
2018-10-23 10:31:17 -0500

    Setup EVN stuff

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,15 @@
 import * as firebase from 'firebase';
 
+//console.log(process.env.FIREBASE_DATABASE_URL);
+//console.log(process.env);
+
 const config = {
-    apiKey: "AIzaSyCkd3d5LQ9eNelnsjYSHU3-b9tp_vlZAqk",
-    authDomain: "expensify-d2b92.firebaseapp.com",
-    databaseURL: "https://expensify-d2b92.firebaseio.com",
-    projectId: "expensify-d2b92",
-    storageBucket: "expensify-d2b92.appspot.com",
-    messagingSenderId: "428482525145"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
 firebase.initializeApp(config);
