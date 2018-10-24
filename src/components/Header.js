f156bac59c6de72a11200230a8c36e7ac6f2a1e4
Your Name <jake.lower17@gmail.com>
2018-10-23 22:18:24 -0500

    auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
         <NavLink activeClassName="is-active" to="/" exact={true}>HOme</NavLink>
         <NavLink activeClassName="is-active" to="/create" exact={true}>create</NavLink>
         <NavLink activeClassName="is-active" to="/help" exact={true}>help</NavLink>
+        <button onClick={startLogout}>Logout</button>
     </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
